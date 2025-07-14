7c2e66147fbb98cbc6f1579cd7009b8f92f737b9
Vandevelde Dimitri <dimitri.vandevelde@hotmail.com>
2025-07-14 17:20:46 +0200

    feat(account): add sign-out functionality to AccountHome screen

--- app/(tabs)/account/index.tsx
+++ app/(tabs)/account/index.tsx
@@ -1,12 +1,20 @@
+import { supabase } from "@/utils/supabase";
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function AccountHome() {
+    const handleSignOut = async () => {
+        const { error } = await supabase.auth.signOut();
+        if (error) {
+            console.error("Erreur de déconnexion :", error.message);
+        }
+    };
     return (
         <View
             style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
             <Text>Bienvenue dans l’espace Compte</Text>
+            <Button onPress={handleSignOut} title="Déconnexion" />
         </View>
     );
 }
